94674ba2db306114fabf79ea875de25cfcd5a0d2
lancelee2885 <lancelee2885@gmail.com>
2022-02-11 00:31:45 -0800

    function: validate if vehicles are valid

--- src/app.js
+++ src/app.js
@@ -14,7 +14,33 @@
  * @return {boolean} A boolean indicating whether input matches expected{Cost,Year}
  */
 function isValid (input, expectedCost, expectedOldestYear) {
-  // Your code here
+
+  try {
+    isValidVehicles(input);
+  } catch(err) {
+    return 'invalid list of vehicles';
+  }
+
+}
+
+/** Helper function checking if given vehicles list is valid. */
+function isValidVehicles(vehicles) {
+  vehicles?.forEach((v) => {
+    if (!v.type || !v.year || !v.value || Object.keys(v).length !== 3) {
+      throw new VehicleError('Invalid vehicle object structure');
+    } else if (!(['Truck', 'Trailer', 'Tractor'].includes(v.type))) {
+      throw new VehicleError(`vehicle type should be one of the following: Truck, Trailer, or Tractor`);
+    } else if (typeof v.year !== 'number') {
+      throw new VehicleError('model year should be a number');
+    } else if (typeof v.value !== 'number') {
+      throw new VehicleError('value should be a number');
+    }
+  })
+  if (vehicles?.length) {
+    return true;
+  } else {
+    throw new VehicleError('No input or an empty array was given.');
+  }
 }
 
 /**
@@ -23,3 +49,5 @@ function isValid (input, expectedCost, expectedOldestYear) {
 function testIsValid () {
   // Your code here
 }
+
+testIsValid();
