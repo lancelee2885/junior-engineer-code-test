3ec45b1177ab7b55604912234bd3aa1a8bb62ed7
lancelee2885 <lancelee2885@gmail.com>
2022-02-11 12:52:49 -0800

    views field added in blog

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 import datetime
 from django.db import models
 
-# def auto_now():
-#     return datetime.datetime.now() - datetime.timedelta(days=1)
+def auto_now():
+    return datetime.datetime.now()
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
@@ -10,6 +10,7 @@ class Post(models.Model):
     posted_by = models.CharField(max_length=50, default="root")
     posted_at = models.DateTimeField(default=datetime.datetime.now)
     edited_at = models.DateTimeField(auto_now=True)
+    views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
