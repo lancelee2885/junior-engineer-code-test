aea13ee95839a5bcd34df4fe9c41e73d309be260
lancelee2885 <lancelee2885@gmail.com>
2022-02-11 13:42:08 -0800

    test: default views and should increment if page visits

--- blog/tests/test_post.py
+++ blog/tests/test_post.py
@@ -1,5 +1,6 @@
 import datetime
 import pytest
+from django.test import Client
 
 from ..models import Post
 
@@ -31,3 +32,21 @@ def test_default_posted_time_is_set():
     assert post.id
     assert post.posted_at is not None
     assert post.posted_at.date() == datetime.datetime.now().date()
+
+@pytest.mark.django_db
+def test_default_post_view_is_0():
+    post = Post(title="Test", text="Testing")
+    post.save()
+
+    assert post.id
+    assert post.views == 0
+
+@pytest.mark.django_db
+def test_default_post_view_increment_by_1():
+    post = Post(title="Test", text="Testing")
+    post.save()
+    client = Client()
+    client.get(f'/blog/{post.id}/')
+    post_views = Post.objects.get(pk=post.id).views
+
+    assert post_views == 1
